e7f36435d33e08ad45ac30ca756fad6c746202b0
osenbal <enbal.blogs215@gmail.com>
2021-02-21 09:47:27 +0700

    appTiketSaya

--- app/src/main/java/com/example/tiketsaya/MyProfileAct.java
+++ app/src/main/java/com/example/tiketsaya/MyProfileAct.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 import com.github.florent37.shapeofview.shapes.CircleView;
@@ -12,6 +13,7 @@ import com.github.florent37.shapeofview.shapes.CircleView;
 public class MyProfileAct extends AppCompatActivity {
 
     LinearLayout item_my_ticket;
+    Button btn_edit_profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,15 @@ public class MyProfileAct extends AppCompatActivity {
         setContentView(R.layout.activity_my_profile);
 
         item_my_ticket = findViewById(R.id.item_my_ticket);
+        btn_edit_profile = findViewById(R.id.btn_edit_profile);
+
+        btn_edit_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoeditprofile = new Intent(MyProfileAct.this,EditProfileAct.class);
+                startActivity(gotoeditprofile);
+            }
+        });
 
         item_my_ticket.setOnClickListener(new View.OnClickListener() {
             @Override
